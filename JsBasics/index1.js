f136262d0fa7749de51ceff0473abdc89354e146
martinengo2108-collab <martinengo2108@gmail.com>
2026-07-16 18:20:30 +0100

    initial commit

---
+++ JsBasics/index1.js
@@ -0,0 +1,14 @@
+ 
+ let name = "Martine";
+ console.log(name);
+let selectorColor="null";
+  console.log(selectorColor);
+let classe ="Uss";
+   console.log(classe);
+let person={
+    age="00",
+    classe ="Uss",
+    Name ="Maida",
+    selectorColor="null"
+}
+console.log(person)
