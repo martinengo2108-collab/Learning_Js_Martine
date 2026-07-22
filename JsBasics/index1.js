aa385a25ccec641f41a385dd81accdbbc7072d6f
martinengo2108-collab <martinengo2108@gmail.com>
2026-07-22 17:14:26 +0100

    functions on Js

--- JsBasics/index1.js
+++ JsBasics/index1.js
@@ -1,14 +1,11 @@
  
- let name = "Martine";
- console.log(name);
-let selectorColor="null";
-  console.log(selectorColor);
-let classe ="Uss";
-   console.log(classe);
-let person={
-    age="00",
-    classe ="Uss",
-    Name ="Maida",
-    selectorColor="null"
+ console.log('Hello World');
+
+
+function happyBirthday(){
+  console.log('Happy birthday to you');
+  console.log('Happy birthday to you');
+  console.log('Happy birthday dear you');
+  console.log('Happy birthday to you');
 }
-console.log(person)
+happyBirthday();
