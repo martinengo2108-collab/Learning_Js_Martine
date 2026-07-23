6c6eb0f3adbc336804dc2b7d10cc7e6c5fd3edca
martinengo2108-collab <martinengo2108@gmail.com>
2026-07-23 12:05:03 +0100

    use of backticks to insert variables

--- JsBasics/index1.js
+++ JsBasics/index1.js
@@ -5,9 +5,9 @@
 function happyBirthday(username, age){
   console.log('Happy birthday to you');
   console.log('Happy birthday to you');
-  console.log('Happy birthday dear $(username) you!');
+  console.log(`Happy birthday dear ${username} you!`);
   console.log('Happy birthday to you');
-  console.log('You are $(age) years old');
+  console.log(`You are ${age} years old`);
 }
 
-happyBirthday("Martine", 0);
+happyBirthday("Martine", 7);
