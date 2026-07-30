760bdecea6fbfdf4e9c7dd1dc820e491227adda9
martinengo2108-collab <martinengo2108@gmail.com>
2026-07-30 20:53:53 +0100

    removing some stylings

--- faq-accordion-main/faq.js
+++ faq-accordion-main/faq.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded" ,()=>{
     const faqContainer = document.querySelector('.container');
 
     faqContainer.addEventListener("click" ,(e) =>{
-        const groupHeader = e.target.closest(".")
+        const groupHeader = e.target.closest(".acc-header")
     })
 });
