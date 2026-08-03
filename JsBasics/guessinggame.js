5b7ded6d2e39aae8d419d5bacc4f36a0021efaa4
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-03 19:06:45 +0100

    using random to constantly change the number

--- JsBasics/guessinggame.js
+++ JsBasics/guessinggame.js
@@ -2,3 +2,43 @@ let randomNumber = Math.floor(Math.random()*100) + 1 ;
 
 let attempts = 0;
 
+let bestScore = localStorage.getItem("bestScore");
+
+if(bestScore){
+    document.getElementById("bestScore")
+    .textContent = bestScore;
+
+}
+const guessInput = document.getElementById("guessInput");
+const message = document.getElementById("message");
+
+const attemptsEl = document.getElementById("attempts");
+
+document.getElementById("guessBtn").addEventListener("click" , ()=>{
+    const guess = Number(guessInput.value);
+
+    if(!guess || guess<1 || guess>100){
+        message.textContent =" Enter a number between 1 and 100";
+
+        message.style.color = "#facc15";
+
+        return;
+    }
+    attempts++;
+
+    attemptsEl.textContent = attempts;
+    if(guess === randomNumber){
+            message.textContent = "🎉Great! The number was $(randomNumber)";
+
+            message.style.color ="#22c55e"
+
+            if(!bestScore || attempts < bestScore){
+
+                localStorage.set
+            }
+
+
+    }
+
+
+})
