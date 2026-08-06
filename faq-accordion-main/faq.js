b809f8091d1cc3dc75451680eee18277c17ebf7b
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-06 16:18:09 +0100

    checks if the clicked FAQ is already open and reset every icon to plus

--- faq-accordion-main/faq.js
+++ faq-accordion-main/faq.js
@@ -12,26 +12,20 @@ headers.forEach(header =>  {
 
         document.querySelectorAll("acc-content").forEach(content =>{
 
-            if(content !== currentContent){
                 content.classList.remove("show");
-            }
-
-
+            
         });
 
         document.querySelectorAll(".open").forEach(icon =>{
-
-            if(icon !==currentIcon){
+            
                 icon.src ="./assets/images/icon-plus.svg";
-            }
+        
         });
-        currentContent.classList.toggle("show");
 
-        if(currentContent.classList.contains("show")){
+        if(isOpen){
+            currentContent.classList.add("show");
+
             currentIcon.src ="./assets/images/icon-minus.svg";
-
-        }else{
-            currentIcon.src ="./assets/images/icon-plus.svg";
         }
     });
 });
