0613709814c04ecff1888e93fc4aab48a6fff2f9
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-06 13:38:33 +0100

    removing the ids acc-items

--- faq-accordion-main/faq.js
+++ faq-accordion-main/faq.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded" ,()=>{
-    const faqContainer = document.querySelector('.container');
-
-    faqContainer.addEventListener("click" ,(e) =>{
-        const groupHeader = e.target.closest(".acc-header")
-    })
-});
+const headers = document.que
