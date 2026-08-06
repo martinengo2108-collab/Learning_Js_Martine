8cd4b7892c0b6309328e71301effe3244bbf3143
martinengo2108-collab <martinengo2108@gmail.com>
2026-08-06 14:56:55 +0100

    adding the javascript code

--- faq-accordion-main/faq.js
+++ faq-accordion-main/faq.js
@@ -1,1 +1,19 @@
-const headers = document.que
+const headers = document.querySelectorAll(".acc-header");
+
+headers.forEach(header =>  {
+    header.addEventListener("click", ()=>{
+    
+        const currentContent = header.nextElementSibling;
+
+        const currentIcon = header.querySelector(".open");
+
+        document.querySelectorAll("acc-content").forEach(content =>{
+
+            if(content !== currentContent){
+                content.classList.remove("show")
+            }
+
+
+        })
+    })
+})
